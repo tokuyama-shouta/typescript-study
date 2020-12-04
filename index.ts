87e447b56d4dae24db580e3218517935cf14ab8e
tokuyama shouta <toku1031ts@gmail.com>
2020-12-04 18:31:13 +0900

    Tuple型を利用して、決まった内容の配列を作る

--- index.ts
+++ index.ts
@@ -35,3 +35,11 @@ console.log(person.name)
 //配列に型をつける
 const fruits: string[] = ['Apple','Banana','Grape']
 
+//Tuple型を使用して、決まった内容の配列を作る方法
+//必ず型注釈する必要がある
+//配列より厳しくなった物
+const book: [string, number, boolean] = ['business',1500,false];
+//pushで追加はできる→TypeScriptは初期値に厳しだけなので
+//出力の時にエラーが出る。
+book.push(1)
+
