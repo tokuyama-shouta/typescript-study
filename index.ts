2a8935ba41ca976775b39067bb522e7b0c3badc1
tokuyama shouta <toku1031ts@gmail.com>
2020-12-04 17:48:01 +0900

    string number boolean

--- index.ts
+++ index.ts
@@ -1,6 +1,16 @@
-let hello: string = 'hello'
-console.log(hello);
 
-function add(a: number, b: number): number {
-  return a + b;
-}
+//TypeScript Study
+
+//boolean(真偽値)
+let hasValue: boolean = true;
+
+//number(数値)
+let count: number = 10;
+let float: number = 3.14;
+let negative: number = -0.12;
+
+//string(文字列)
+let single: string = 'hello';
+let double: string = "hello";
+let back: string = `hello`;
+
