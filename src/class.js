a4860286cda710ecf73e476e1cb0ab1402490c22
tokuyama shouta <toku1031ts@gmail.com>
2020-12-07 20:45:18 +0900

    クラスにメソッドを追加する方法

--- src/class.js
+++ src/class.js
@@ -3,6 +3,14 @@ class Person {
     constructor(initName) {
         this.name = initName;
     }
+    greeting() {
+        console.log(`Hello! My name is ${this.name}`);
+    }
 }
 const quill = new Person('Quill');
-console.log(quill);
+quill.greeting();
+const anotherQuill = {
+    name: 'anotherQuill',
+    anotherGreeting: quill.greeting
+};
+anotherQuill.anotherGreeting();
--- src/class.ts
+++ src/class.ts
@@ -1,9 +1,20 @@
 
 class Person {
+
   name: string;
   constructor(initName: string) {
     this.name = initName;
   }
+  
+  greeting(this: {name: string}) {
+    console.log(`Hello! My name is ${this.name}`);
 }
+}
+
 const quill = new Person('Quill');
-console.log(quill);
+quill.greeting();
+const anotherQuill = {
+    name: 'anotherQuill',
+    anotherGreeting: quill.greeting
+}
+anotherQuill.anotherGreeting();
