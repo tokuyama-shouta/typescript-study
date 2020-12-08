886ebd7802ad3769883411c5190b47835212b3a6
tokuyama shouta <toku1031ts@gmail.com>
2020-12-08 16:36:19 +0900

    public private

--- src/class.ts
+++ src/class.ts
@@ -2,20 +2,23 @@
 class Person {
 
   name: string;
-  constructor(initName: string) {
+  private age: number;
+
+  constructor(initName: string,initAge: number) {
     this.name = initName;
+    this.age = initAge;
+  }
+
+  incrementAge(){
+    this.age += 1;
   }
 
   greeting(this: Person) {
-    console.log(`Hello! My name is ${this.name}`);
+    console.log(`Hello! My name is ${this.name}. I am ${this.age} years old.`);
   }
 }
 
 let Person2: Person;
-const quill = new Person('Quill');
+const quill = new Person('Quill',38);
+quill.incrementAge();
 quill.greeting();
-const anotherQuill = {
-    name: 'anotherQuill',
-    greeting: quill.greeting
-}
-anotherQuill.greeting();
