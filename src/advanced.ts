ed419f9fbd012bb0f61edd552ac4a7fabe0abd50
tokuyama shouta <toku1031ts@gmail.com>
2020-12-10 16:01:10 +0900

    タグ付きUnion

--- src/advanced.ts
+++ src/advanced.ts
@@ -35,11 +35,13 @@ function describeProfile(nomadWorker: NomadWorker){
   }
 }
 class Dog {
+  kind: 'dog' = 'dog'
   speak() {
     console.log('bow-wow');
   }
 }
 class Bird {
+  kind: 'bird' = 'bird'
   speak() {
     console.log('tweet-tweet');
   }
@@ -50,6 +52,10 @@ class Bird {
 type Pet = Dog | Bird;
 function havePet(pet: Pet){
   pet.speak();
+  switch (pet.kind) {
+    case 'bird':
+      pet.fly();
+  }
   if(pet instanceof Bird){
     pet.fly();
   }
