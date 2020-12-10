7319c1a19faa7aaed1403f36ef6a3d35d6352817
tokuyama shouta <toku1031ts@gmail.com>
2020-12-10 20:39:58 +0900

    関数のオーバーロード

--- src/advanced.ts
+++ src/advanced.ts
@@ -17,12 +17,16 @@ type  NumberBoolean = number | boolean;
 type StringNumber = string | number;
 type Mix = NumberBoolean & StringNumber;
 
-function toUpperCase(x: string | number){
+function toUpperCase(x: string): string;
+function toUpperCase(x: number): number;
+function toUpperCase(x: string | number): string | number {
   if (typeof x === 'string') {
     return x.toUpperCase();
   }
-  return '';
+  return x;
 }
+const upperHello = toUpperCase('hello');
+
 type NomadWorker = Engineer | Blogger;
 function describeProfile(nomadWorker: NomadWorker){
   console.log(nomadWorker.name);
