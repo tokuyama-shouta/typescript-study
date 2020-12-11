0d1644f30da629ee3b39efa8ad5447b2f4dd5e31
tokuyama shouta <toku1031ts@gmail.com>
2020-12-11 17:46:42 +0900

    関数型のユニオン型

--- src/advanced.ts
+++ src/advanced.ts
@@ -30,15 +30,25 @@ interface TmpFunc {
   (x: number): number;
 }
 const upperHello: TmpFunc = function (x: string | number) { return 0};
+// interface FuncA {
+//   (a: number, b: string): number;
+//   (a: string, b: number): number;
+// }
+// interface FuncB {
+//   (a: string): number;
+// }
+// let intersectionFunc: FuncA & FuncB;
+// intersectionFunc = function(a: number | string, b?: number | string) {return 0}
+
 interface FuncA {
-  (a: number, b: string): number;
-  (a: string, b: number): number;
+  (a: number): number;
 }
 interface FuncB {
-  (a: string): number;
+  (a: string): string;
 }
-let intersectionFunc: FuncA & FuncB;
-intersectionFunc = function(a: number | string, b?: number | string) {return 0}
+let unionFunc: FuncA | FuncB;
+unionFunc = function (a: number) { return 34}
+
 
 type NomadWorker = Engineer | Blogger;
 function describeProfile(nomadWorker: NomadWorker){
