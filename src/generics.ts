08a89626f5c94f99706c7fed966d55509b717ab3
tokuyama shouta <toku1031ts@gmail.com>
2020-12-11 19:36:39 +0900

    Classに対してジェネリクスを使用する方法

--- src/generics.ts
+++ src/generics.ts
@@ -4,3 +4,21 @@ function copy<T extends {name: string}, U extends keyof T>(value: T, key: U): T 
   return value;
 }
 console.log(copy({name: 'Quill', age: 38}, 'age'));
+
+class LightDatabase<T extends string | number | boolean> {
+  private data: T[] = [];
+  add(item: T) {
+    this.data.push(item);
+  }
+  remove(item: T) {
+    this.data.splice(this.data.indexOf(item),1)
+  }
+  get() {
+    return this.data;
+  }
+}
+const stringLightDatabase = new LightDatabase<string>();
+stringLightDatabase.add('Apple');
+stringLightDatabase.add('Banana');
+stringLightDatabase.add('Greap');
+stringLightDatabase.remove('Banana');
