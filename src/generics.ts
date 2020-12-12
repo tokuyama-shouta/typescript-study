6478570fc86e05bce33bca1e703dfa2ff9d6b08f
tokuyama shouta <toku1031ts@gmail.com>
2020-12-12 12:49:09 +0900

    デフォルトの型パラメーターを指定する

--- src/generics.ts
+++ src/generics.ts
@@ -31,3 +31,26 @@ const tmpDatabase: TmpDatabase<number> = {
   id: 3,
   data: [32]
 }
+
+interface Todo {
+  title: string;
+  text: string;
+}
+type Todoable = Partial<Todo>
+type ReadTodo = Readonly<Todo>
+
+const fetchData: Promise<string> = new Promise(resolve => {
+  setTimeout(() => {
+    resolve('hello');
+  },3000);
+})
+fetchData.then(data => {
+  data.toUpperCase();
+})
+const vegetables: Array<string> = ['Tomato','Broccoli','Asparagus'];
+
+interface ResponseData<T extends { message: string} = any> {
+  data: T;
+  status: number;
+}
+let tmp2: ResponseData;
